ae3a7c424a8cfbfc5ab381a2a79e545a4a4f9c9d
muhammad zain <zainansari887@gmail.com>
2024-06-01 20:19:44 +0500

    files update

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS to allow requests from 'http://localhost:3000'
+  app.enableCors({
+    origin: 'http://localhost:5173', // Replace with the actual origin of your frontend application
+  });
 
   const config = new DocumentBuilder()
     .setTitle('E-commerce API')
